cdb8b7aab67af43af9f1ca6792225c80bdacf692
firmends <firmends@yahoo.com>
2022-08-28 17:39:18 -0500

    First Commit

--- BugTrackerUI/Bug.cs
+++ BugTrackerUI/Bug.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace BugTrackerUI
 {
-    public class Bug
-    {
-        public int Id { get; set; }
+  public class Bug
+  {
+    public int Id { get; set; }
 
-        public string Title { get; set; }
+    public string Title { get; set; } = string.Empty;
 
-        public string Description { get; set; }
+    public string Description { get; set; } = string.Empty;
 
-        public int Priority { get; set; }
-    }
+    public int Priority { get; set; }
+  }
 }
